2dff53fb9435569677d589152400a59b53a68b42
Ramkrishna Roy <76205677+Ramkrishna307@users.noreply.github.com>
2024-01-25 10:01:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+  // Define a regular expression pattern for a valid email address
+  const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  // Check if the input string matches the email pattern
+  return pattern.test(str.trim());
+
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt("Enter an email address.")
 alert(validEmail(str));
